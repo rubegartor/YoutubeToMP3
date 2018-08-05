8ef1c746e5ae0465d1089fb5e275f57bac198e71
rubegartor <rubegartor@gmail.com>
2018-08-05 23:27:45 +0200

    Update application

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request, send_file
-from urllib.request import urlopen
+from flask import Flask, render_template, request, send_file, flash
+from bs4 import BeautifulSoup as bs
+import requests
 import youtube_dl
 import string
 import random
@@ -8,6 +9,7 @@ import time
 import os
 
 app = Flask(__name__)
+app.secret_key = 'key'
 
 def internal_error(e):
   return render_template('e500.html')
@@ -63,6 +65,30 @@ def main():
 def index():
   return render_template('index.html')
 
+@app.route('/playlist', methods = ['GET', 'POST'])
+def playlist():
+  if request.method == 'POST':
+    r = requests.get(request.form['playlistURL'])
+    soup = bs(r.text, 'html.parser')
+    res = soup.find_all('a',{'class':'pl-video-title-link'})
+    vid_id = [l.get('href').split('=')[1].split('&')[0] for l in res]
+
+    path = generatePath(32)
+
+    for id in vid_id:
+      req = requests.get('https://www.youtube.com/watch?v=' + id)
+      if req.status_code == requests.codes['ok']:
+        try:
+          download_mp3('https://www.youtube.com/watch?v=' + id, path)
+        except:
+          #No se ha podido descargar el siguiente enlace
+          continue
+
+    zipFile('static/downloads/{}'.format(path), 'static/downloads/{}'.format(path))
+    return send_file('static/downloads/{}.zip'.format(path), attachment_filename="playlist.zip")
+
+  return render_template('playlist.html')
+
 @app.route('/songs', methods = ['POST'])
 def download():
   _url = list(set([line.strip() for line in request.form['inputURL'].split('\r\n') if line.strip() != '' and line.startswith('https://www.youtube.com/watch?v=')]))
@@ -71,15 +97,15 @@ def download():
     path = generatePath(32)
 
     for song in _url:
-      with urlopen(song) as conn:
-        if conn.code == 200:
-          try:
-            download_mp3(song, path)
-          except:
-            #No se ha podido descargar el siguiente enlace
-            continue
-        else:
-          pass
+      req = requests.get(song)
+      if req.status_code == requests.codes['ok']:
+        try:
+          download_mp3(song, path)
+        except:
+          #No se ha podido descargar el siguiente enlace
+          continue
+      else:
+        pass
 
     zipFile('static/downloads/{}'.format(path), 'static/downloads/{}'.format(path))
     return send_file('static/downloads/{}.zip'.format(path), attachment_filename="songs.zip")
